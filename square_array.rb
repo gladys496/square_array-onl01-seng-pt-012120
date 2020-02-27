e945f176662fbcc6b37c7790adda92e61989a11e
Gladys Gutierrez <gutierrezgladys721@gmail.com>
2020-02-27 20:30:49 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,9 @@
-def square_array(numbers)
-numbers = [1,2,3,4]
-array.each do |number|
-  puts "Today my number is #{number}."
 
-end 
 
+def square_array(array)
+  square_array = [1, 2, 3, 4]
+  square_array.each do |array|
+    puts "Today my number is #{array}."
+  end 
+  square_array.
+end
